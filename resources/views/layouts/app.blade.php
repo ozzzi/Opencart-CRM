6fc1658b2693def93062fa7011c9165cb1ce24ec
ozzzi <osinezator.666@gmail.com>
2023-12-23 14:55:28 +0200

    feature(Admin) change admin styles

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8" />
+
+        <meta name="application-name" content="{{ config('app.name') }}" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>{{ config('app.name') }}</title>
+
+        <style>
+            [x-cloak] {
+                display: none !important;
+            }
+        </style>
+
+        @vite('resources/css/app.css')
+    </head>
+
+    <body class="antialiased"
+          x-data="{ page: 'ecommerce', 'loaded': true, 'darkMode': true, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }">
+        <div class="flex h-screen overflow-hidden">
+            @include('partials.sidebar')
+
+            <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                @include('partials.header')
+
+                <main>
+                    <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                        @yield('content')
+                    </div>
+                </main>
+
+            </div>
+
+            @vite('resources/js/app.js')
+        </div>
+    </body>
+</html>
--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: [
                 ...refreshPaths,
-                'app/Filament/**',
-                'app/Forms/Components/**',
                 'app/Livewire/**',
-                'app/Infolists/Components/**',
-                'app/Providers/Filament/**',
-                'app/Tables/Columns/**',
             ],
         }),
     ],
