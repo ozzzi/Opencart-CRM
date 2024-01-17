6d24ff948b23367d0363c8dfda50a64d55053463
ozzzi <osinezator.666@gmail.com>
2024-01-17 19:09:13 +0200

    feature(Traits) add json response trait

---
+++ app/Support/Traits/JsonResponser.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Support\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+trait JsonResponser
+{
+    protected function success($data = [], string $message = null, int $code = 200): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+    protected function error(string $message, int $code, $data = null): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+}
