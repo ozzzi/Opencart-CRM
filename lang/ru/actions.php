832ac7399f2874e74f68b41d1da18926758048b6
ozzzi <osinezator.666@gmail.com>
2024-01-28 12:53:58 +0200

    feature(Translation) add translations

---
+++ lang/ru/actions.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'create' => 'Создать',
+    'creating' => 'Создание',
+    'save' => 'Сохранить',
+    'edit' => 'Редактировать',
+    'editing' => 'Редактирование',
+    'delete' => 'Удалить',
+];
---
+++ lang/ru/fields.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'name' => 'Имя',
+    'phone' => 'Телефон',
+    'email' => 'Email',
+    'address' => 'Адрес',
+    'comment' => 'Комментарий',
+    'is_bad' => 'Проблемный клиент',
+    'client' => 'Клиент',
+    'type' => 'Тип',
+    'contact' => 'Контакт',
+    'contacts' => 'Контакты',
+    'no_choose' => 'Выберете',
+    'store' => 'Магазин',
+];
