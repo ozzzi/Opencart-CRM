ea4ba609bd9f98bf51a2d8b7d88e2621d6287590
ozzzi <osinezator.666@gmail.com>
2024-09-08 14:46:28 +0300

    fix(Notification): change chat id type

--- app/Listeners/Notification/OrderNotification.php
+++ app/Listeners/Notification/OrderNotification.php
@@ -18,7 +18,7 @@ class OrderNotification
 
         $this->notificationService->setService(new TelegramNotification(
             config('services.telegram.token'),
-            config('services.telegram.chat_id')
+            (int) config('services.telegram.chat_id')
         ));
 
         $this->notificationService->notify($message, 'Новый заказ');
